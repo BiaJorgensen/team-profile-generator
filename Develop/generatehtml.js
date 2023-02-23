e8160057337ab097207b64e26d54a6025980d2cd
Bia Jorgensen <BiaJorgensen@users.noreply.github.com>
2023-02-22 20:59:53 -0600

    Added link to bootstrap and changed tab name

--- Develop/generatehtml.js
+++ Develop/generatehtml.js
@@ -6,7 +6,8 @@ function generatehtml(teamMembers) {
         <meta charset="UTF-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Document</title>
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css">
+        <title>My Team</title>
     </head>
     <body>
     <div>
