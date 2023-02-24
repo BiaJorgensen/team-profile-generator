c4498e3cd51bfd468a9f42b4aa20d2ad9a98e309
Bia Jorgensen <BiaJorgensen@users.noreply.github.com>
2023-02-23 18:06:54 -0600

    Fixed comment

--- Develop/generatehtml.js
+++ Develop/generatehtml.js
@@ -7,14 +7,14 @@ function generatehtml(teamMembers) {
         <meta charset="UTF-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        // Link to Bootstrap
+    <!-- Link to Bootstrap -->
     <link
       href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css"
       rel="stylesheet"
       integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3"
       crossorigin="anonymous"
     />
-    // Link to CSS file
+    <!-- Link to CSS file -->
     <link rel="stylesheet" href="./style.css">
         <title>My Team</title>
     </head>
